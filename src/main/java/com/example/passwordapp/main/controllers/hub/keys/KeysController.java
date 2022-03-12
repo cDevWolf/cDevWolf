42033a9128b4b358d89a95cab84c8070bcfc824d
cDevWolf <uwu0016xdhe@gmail.com>
2022-03-12 11:18:55 -0600

    pre-alpha v1.1.2 key?

--- src/main/java/com/example/passwordapp/main/controllers/hub/keys/KeysController.java
+++ src/main/java/com/example/passwordapp/main/controllers/hub/keys/KeysController.java
@@ -1,14 +1,20 @@
 package com.example.passwordapp.main.controllers.hub.keys;
 
+import com.example.passwordapp.models.service.item.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/keys")
 public class KeysController {
+    @Autowired
+    private ItemService itemService;
     @GetMapping("/all-keys")
-    public String goToAllKeys(){
+    public String goToAllKeys(Model model){
+        model.addAttribute("items", itemService.getAllItems());
         return "allkeys";
     }
 }
